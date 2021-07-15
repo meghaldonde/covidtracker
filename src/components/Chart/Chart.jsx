89258b0502d4b4ad535599801c4828073cf4303c
meghaldonde <meghal.donde@gmail.com>
2021-07-15 19:53:44 -0400

    added bar chart

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+
+
+import styles from './Chart.module.css';
+
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
+
+
+  const barChart = (
+    confirmed ? (
+      <Bar
+        data={{
+          labels: ['Infected', 'Recovered', 'Deaths'],
+          datasets: [
+            {
+              label: 'People',
+              backgroundColor: ['rgba(255, 196, 0, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'],
+              data: [confirmed.value, recovered.value, deaths.value],
+            },
+          ],
+        }}
+        options={{
+          legend: { display: false },
+          title: { display: true, text: `Current state in ${country}` },
+        }}
+      />
+    ) : null
+  );
+
+
+
+
+  return (
+    <div className={styles.container}>
+      { barChart}
+    </div>
+  );
+};
+
+export default Chart;
